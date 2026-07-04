58a602f7f01ece6438a31216baccd5e1e57f3f6e
Lucas Silva <lucassiso81@gmail.com>
2026-07-03 22:13:46 -0300

    Doctors test

--- src/main/java/com/example/projectspring/configuration/ConfigTest.java
+++ src/main/java/com/example/projectspring/configuration/ConfigTest.java
@@ -1,6 +1,12 @@
 package com.example.projectspring.configuration;
 
+import com.example.projectspring.entities.Appointment;
+import com.example.projectspring.entities.Doctor;
+import com.example.projectspring.entities.Hospital;
 import com.example.projectspring.entities.Patient;
+import com.example.projectspring.repositories.AppointmentRepository;
+import com.example.projectspring.repositories.DoctorRepository;
+import com.example.projectspring.repositories.HospitalRepository;
 import com.example.projectspring.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +20,12 @@ import java.util.Arrays;
 public class ConfigTest implements CommandLineRunner {
     @Autowired
     private PatientRepository patientRepository;
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+    @Autowired
+    private DoctorRepository doctorRepository;
+    @Autowired
+    private HospitalRepository hospitalRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -21,6 +33,14 @@ public class ConfigTest implements CommandLineRunner {
         Patient p2 = new Patient(null,"POrra da silva", "787888", "11988877666");
 
         patientRepository.saveAll(Arrays.asList(p1,p2));
+
+        Doctor d1 = new Doctor(null,"Joseli nho","2662662");
+        Doctor d2 = new Doctor(null,"Piconha fres","2454322");
+
+        doctorRepository.saveAll(Arrays.asList(d1,d2));
+
+        Hospital h1 = new Hospital();
+
 
     }
 }
--- src/main/java/com/example/projectspring/entities/Doctor.java
+++ src/main/java/com/example/projectspring/entities/Doctor.java
@@ -1,8 +1,12 @@
 package com.example.projectspring.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "doctors")
 public class Doctor {
     @Id
@@ -10,6 +14,12 @@ public class Doctor {
     private Long id;
     private String name;
     private String crm;
+
+    public Doctor(Long id, String name, String crm) {
+        this.id = id;
+        this.name = name;
+        this.crm = crm;
+    }
 
     @OneToOne(mappedBy = "doctor",cascade = CascadeType.ALL)
     private Appointment appointment;
