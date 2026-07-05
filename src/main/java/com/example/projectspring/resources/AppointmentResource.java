aa83b98df9562f575119ee13cb139854041f9152
Lucas Silva <lucassiso81@gmail.com>
2026-07-05 00:36:32 -0300

    Implemented methods insert, delete and update in appointments

--- src/main/java/com/example/projectspring/resources/AppointmentResource.java
+++ src/main/java/com/example/projectspring/resources/AppointmentResource.java
@@ -4,11 +4,10 @@ import com.example.projectspring.entities.Appointment;
 import com.example.projectspring.services.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,6 +24,23 @@ public class AppointmentResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Appointment> findById(@PathVariable Long id){
         Appointment obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    @PostMapping
+    public ResponseEntity<Appointment> insert(@RequestBody Appointment obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Appointment> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Appointment> update(@PathVariable Long id,@RequestBody Appointment obj){
+        obj = service.update(id,obj);
         return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/example/projectspring/services/AppointmentService.java
+++ src/main/java/com/example/projectspring/services/AppointmentService.java
@@ -2,8 +2,11 @@ package com.example.projectspring.services;
 
 import com.example.projectspring.entities.Appointment;
 import com.example.projectspring.repositories.AppointmentRepository;
+import com.example.projectspring.services.exceptions.DatabaseException;
 import com.example.projectspring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +23,30 @@ public class AppointmentService {
     public Appointment findById(Long id){
         Optional<Appointment> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+    public Appointment insert(Appointment obj){
+        return repository.save(obj);
+    }
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+    public Appointment update(Long id,Appointment obj){
+        try {
+            Appointment entity = repository.getReferenceById(id);
+            updateData(entity,obj);
+            return repository.save(entity);
+        }catch (ResourceNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+    private void updateData(Appointment entity,Appointment obj){
+        entity.setAppointmentDate(obj.getAppointmentDate());
     }
 
 }
